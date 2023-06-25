cc557a7659a8c4ed1ea0f0c7862f9a22c184789b
105mm <king7532@knou.ac.kr>
2023-06-25 15:19:50 +0900

    commit

---
+++ nadocoding/6여러문장 주석처리.py
@@ -0,0 +1,25 @@
+# 애완동물을 소개해 주세요~
+
+# animal="강아지"
+# name="연탄이"
+# age=4
+# hobby="산책"
+# is_adult=age>=3
+
+#ctrl + / = 여러 문장 블록설정 후 한번에 주석처리
+
+''' 
+
+print("우리집 "+animal+" 이름은 "+name+" 이예요")
+hobby = "공놀이" #위에서는 산책 밑에서는 공놀이
+print(name+"는 "+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
+print(name,"는 ",(age),"살이며, "+hobby+"을 아주 좋아해요") #+대신 ,을 쓰면 str이 필요없지만 무조건 띄어쓰기가 된다
+print(name+"는 어른일까요? "+str(is_adult))
+print(name,"는 어른일까요? ",is_adult)
+
+#정수형 age는 str로 감싸줘야 문자열로 출력가능
+#boolean형도 str로 감싸줘야 문자열로 출력가능
+
+'''
+
+#''' ''' 여러 문장 묶어서 주석처리
