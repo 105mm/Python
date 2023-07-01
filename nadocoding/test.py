d829df0c6d19a61613dd5bb12ce70548e12c2376
105mm <king7532@knou.ac.kr>
2023-07-01 16:23:22 +0900

    commit

--- nadocoding/test.py
+++ nadocoding/test.py
@@ -1,7 +1,2 @@
-time = int(input())
-
-for i in range(1, time + 1):
-    a, b = map(int, input().split())
-    c = a + b
-
-    print("Case #"+str(i)+": "+str(a)+" + "+str(b)+" = "+str(c))
+print("""30
+king7532""")
